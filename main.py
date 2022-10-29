909775e091dfe83832470e3cfd724b077bc3c939
unknown <a.marquez.m.89@gmail.com>
2022-10-29 12:51:35 +0200

    finalizado

---
+++ main.py
@@ -0,0 +1,12 @@
+import operaciones
+import operaciones.operaciones as opreaciones
+
+def main():
+    print("Realizando suma ", opreaciones.suma(4,5))
+    print("Realizando resta", opreaciones.resta(5,4))
+    print("Realizando multiplicacion", opreaciones.multiplica(5,4))
+    print("Realizando divide", opreaciones.divide(5,4))   
+
+
+if __name__ == '__main__':
+    main()
